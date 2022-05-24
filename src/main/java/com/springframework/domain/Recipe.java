44c3c2da91bdc6dc0727884e107b3f0ddd00324e
Nikola Smilyanski <96414322+nsmilyanski@users.noreply.github.com>
2022-05-24 11:10:47 +0300

    Adding a Ingredient and make relation with Recipe.

--- src/main/java/com/springframework/domain/Recipe.java
+++ src/main/java/com/springframework/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.springframework.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -18,6 +19,9 @@ public class Recipe {
     private String directions;
     //todo add
     //private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
