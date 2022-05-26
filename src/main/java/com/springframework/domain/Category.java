e1e2386118fcceb303b7da0e6b900a763c90b9ad
Nikola Smilyanski <96414322+nsmilyanski@users.noreply.github.com>
2022-05-26 12:32:49 +0300

    Add a lombok dependency and annotations in the classes.

--- src/main/java/com/springframework/domain/Category.java
+++ src/main/java/com/springframework/domain/Category.java
@@ -1,10 +1,14 @@
 package com.springframework.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Data
 @Entity()
+@EqualsAndHashCode(exclude = {"recipes"})
 public class Category {
 
     @Id
@@ -16,27 +20,4 @@ public class Category {
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes = new HashSet<>();
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Set<Recipe> getRecipes() {
-        return recipes;
-    }
-
-    public void setRecipes(Set<Recipe> recipes) {
-        this.recipes = recipes;
-    }
 }
