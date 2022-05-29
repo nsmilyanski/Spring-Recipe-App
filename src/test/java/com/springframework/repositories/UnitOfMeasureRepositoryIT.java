d8f54a1362c7fc359cb4a772b82275ee9c543583
Nikola Smilyanski <96414322+nsmilyanski@users.noreply.github.com>
2022-05-29 16:56:10 +0300

    Add a IT test for UnitOfMeasureRepository.

---
+++ src/test/java/com/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,41 @@
+package com.springframework.repositories;
+
+import com.springframework.domain.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void findByDescription() throws Exception {
+
+        Optional<UnitOfMeasure> teaspoon = unitOfMeasureRepository.findUnitOfMeasureByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", teaspoon.get().getDescription());
+    }
+
+    @Test
+    public void findByDescriptionCup() throws Exception {
+
+        Optional<UnitOfMeasure> teaspoon = unitOfMeasureRepository.findUnitOfMeasureByDescription("Cup");
+
+        assertEquals("Cup", teaspoon.get().getDescription());
+    }
+}
